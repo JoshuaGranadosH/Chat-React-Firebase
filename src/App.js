c94651abf03c817f699e63a55194f07500502c6f
Josh Granados <josh_ramstein@hotmail.com>
2021-05-13 00:55:56 -0500

    Final chat with firebase

--- src/App.js
+++ src/App.js
@@ -52,7 +52,6 @@ function SignIn() {
   return (
     <>
       <button className="sign-in" onClick={signInWithGoogle}>Sign in with Google</button>
-      <p>Do not violate the community guidelines or you will be banned for life!</p>
     </>
   )
 
@@ -102,7 +101,7 @@ function ChatRoom() {
 
     <form onSubmit={sendMessage}>
 
-      <input value={formValue} onChange={(e) => setFormValue(e.target.value)} placeholder="say something nice" />
+      <input value={formValue} onChange={(e) => setFormValue(e.target.value)} placeholder="Di algo bonito :3" />
 
       <button type="submit" disabled={!formValue}> <span role="img" aria-label="cart">📩</span></button>
 
